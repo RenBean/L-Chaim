050e42cd2f0a45ae0e963e12538501231a8e13b3
Ari Dalton <ariann.dalton@gmail.com>
2017-03-27 11:56:22 -0600

    first commit - template styled, bootstrap navigation styled

---
+++ header.js
@@ -0,0 +1,78 @@
+document.write(
+    '   <div class="navbar navbar-default navbar-fixed-top" role="navigation">' +
+    '       <div class="container">' +
+    '           <div class="navbar-header">' +
+    '               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">' +
+    '               <span class=="sr-only"></span>' +
+    '               <span class="icon-bar"></span>' +
+    '               <span class="icon-bar"></span>' +
+    '               <span class="icon-bar"></span>' +
+    '               </button>' +
+    '               <a href="#" class="navbar-brand"></a>' +
+    '               <a href="#" class="navbar-left"></a>' +
+    '           </div>' +
+    '           <div class="navbar-collapse collapse">' +
+    '           <img class="top-1-4" src="main-header.png" />' +
+    '               <ul class="nav navbar-nav navbar-right">' +
+    '                   <li><a href="#">Home</a></li>' +
+    '                   <li><a href="#">Classes</a></li>' +
+    '                   <li class="dropdown">' +
+    '                       <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products<b class="caret"></b></a>' +
+    '                       <ul class="dropdown-menu">' +
+    '                           <li class="dropdown-header">Joan\'s Inventory</li>' +
+    '                           <li><a href="#">Books</a></li>' +
+    '                           <li><a href="#">DVD\'s</a></li>' +
+    '                           <li><a href="#">Essential Oils</a></li>' +
+    '                           <li><a href="#">System Builders</a></li>' +
+    '                           <li><a href="#">Needak Rebounders</a></li>' +
+    '                           <li class="dropdown-header">Global Healing Center Shop</li>' +
+    '                           <li><a href="#">Allertrex</a></li>' +
+    '                           <li><a href="#">Detoxadine</a></li>' +
+    '                           <li><a href="#">Livatrex</a></li>' +
+    '                           <li><a href="#">Oxy-Powder</a></li>' +
+    '                           <li><a href="#">Turmeric</a></li>' +
+    '                           <li><a href="#">VeganSafe B-12</a></li>' +
+    '                       </ul>' +
+    '                   </li>' +
+    '                   <li class="dropdown">' +
+    '                       <a href="#" class="dropdown-toggle" data-toggle="dropdown">Articles<b class="caret"></b></a>' +
+    '                       <ul class="dropdown-menu">' +
+    '                           <li class="dropdown-header">Healthy Habits</li>' +
+    '                           <li><a href="#">Diatomaceous Earth</a></li>' +
+    '                           <li><a href="#">Health Through Nutrition</a></li>' +
+    '                           <li><a href="#">Benefits of Juicing</a></li>' +
+    '                           <li><a href="#">Whole Food Nutrition</a></li>' +
+    '                           <li><a href="#">Improving Your Diet</a></li>' +
+    '                           <li><a href="#">Understanding Detoxificatin</a></li>' +
+    '                           <li><a href="#">Detoxification</a></li>' +
+    '                           <li><a href="#">Antidepressants</a></li>' +
+    '                           <li><a href="#">High Blood Pressure</a></li>' +
+    '                           <li><a href="#">ACID-the Trojan Horse</a></li>' +
+    '                           <li><a href="#">Emotional Healing</a></li>' +
+    '                       </ul>' +
+    '                   </li>' +
+    '                   <li class="dropdown">' +
+    '                       <a href="#" class="dropdown-toggle" data-toggle="dropdown">Links<b class="caret"></b></a>' +
+    '                       <ul class="dropdown-menu">' +
+    '                           <li class="dropdown-header">Resources</li>' +
+    '                           <li><a href="#">ChooseLife.JuicePlus.com</a></li>' +
+    '                           <li><a href="#">YoungLiving.com/KLMFamily</a></li>' +
+    '                           <li><a href="#">www.StandardProcess.com</a></li>' +
+    '                           <li><a href="#">www.BrainAdvance.org</a></li>' +
+    '                       </ul>' +
+    '                   </li>' +
+    '                   <li><a href="#">Contact</a></li>' +
+    '                   <li><a href="#">E-mail</a></li>' +
+    // '                   <li><a href="#">Healthy Habits Checklist</a></li>' +
+    // '                   <li class="dropdown">' +
+    // '                       <a href="#" class="dropdown-toggle" data-toggle="dropdown">Product Catalog<b class="caret"></b></a>' +
+    // '                       <ul class="dropdown-menu">' +
+    // '                           <li class="dropdown-header">Download</li>' +
+    // '                           <li><a href="#">catalog.doc</a></li>' +
+    // '                       </ul>' +
+    // '                   </li>' +
+    '               </ul>' +
+    '           </div>' +
+    '       </div>' +
+    '    </div>'
+);
